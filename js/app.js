b7c2a5ef068f20fe02b51ead9cd5aeac6ff89194
__lesovikova__ <__lesovikova@gmail.com__>
2023-07-06 15:48:37 +0100

    add date function

--- js/app.js
+++ js/app.js
@@ -2,6 +2,15 @@
 
 let state = new AppState();
 state.takeFromLocalStorage();
+compareDays(state.allDays)
 
 form.addEventListener("submit", getResults);
 
+function compareDays(data){
+    const d = new Date(); 
+    const date = `${d.getDate()}.${d.getMonth()+1}.${d.getFullYear()}`;
+    console.log(date);
+    if(data.find(e => e.date === date)){
+        showFinish();
+    }
+}
